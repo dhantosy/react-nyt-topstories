249f1cf8fd28bdb0dd253f9f9da6005ff005f0e7
dhantosy <dhantosy@gmail.com>
2019-01-09 14:17:57 +0700

    (feat) added Header and Footer components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const footer = () => {
+  return ( 
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <span>DSY@2019</span>
+          </div>
+        </div>
+      </div>
+    </footer>
+    )
+}
+
+export default footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const header = () => {
+  return ( 
+    <header>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>New York Times - Top Stories</h1> 
+          </div>
+        </div>
+      </div>
+    </header>
+    )
+}
+
+export default header;
