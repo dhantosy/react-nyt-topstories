9845e8909ba89aac8733db379d9ed6d2195a046e
dhantosy <dhantosy@gmail.com>
2019-01-09 22:37:23 +0700

    (feat) get props from Navigation and pass props to News

--- src/App.js
+++ src/App.js
@@ -14,14 +14,20 @@ class App extends Component {
     this.setState({ layout: aLayout });
   }
 
+  handleCatChange = (aValue) => {
+    this.setState({ category: aValue });
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
         <Navigation
-          layoutChange={this.handleLayoutChange} />
+          layoutChange={this.handleLayoutChange}
+          catChange={this.handleCatChange} />
         <News 
-          layout={this.state.layout} />
+          layout={this.state.layout} 
+          category={this.state.category}/>
         <Footer />
       </div>
     );
