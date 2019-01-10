0f8cf07d03b319fba4b2908cb0901281a621488d
dhantosy <dhantosy@gmail.com>
2019-01-10 12:52:02 +0700

    Updates

---
+++ precache-manifest.a430fac2fb3c31cf241d22b6502803a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "10c1126864feb9c17add",
+    "url": "/react-nyt-topstories/static/js/runtime~main.10c11268.js"
+  },
+  {
+    "revision": "c425620b7409e211262c",
+    "url": "/react-nyt-topstories/static/js/main.c425620b.chunk.js"
+  },
+  {
+    "revision": "0d53b7c0f00e115e2266",
+    "url": "/react-nyt-topstories/static/js/1.0d53b7c0.chunk.js"
+  },
+  {
+    "revision": "c425620b7409e211262c",
+    "url": "/react-nyt-topstories/static/css/main.d24a5b50.chunk.css"
+  },
+  {
+    "revision": "0d53b7c0f00e115e2266",
+    "url": "/react-nyt-topstories/static/css/1.31da5ad4.chunk.css"
+  },
+  {
+    "revision": "0e777fef1548e73c110c86356b8a9120",
+    "url": "/react-nyt-topstories/index.html"
+  }
+];
